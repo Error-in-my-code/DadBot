22d65177301b349b3eaa1e390778ea485bc65724
corn <callofduty54682@gmail.com>
2024-02-20 23:38:51 -0400

    Added file reader from token.txt, root proj folder, not src

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,18 +5,39 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+// for reading the secret; take it out if you don't use it or something idc
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 import javax.security.auth.login.LoginException;
 
 public class Main {
     public static void main(String[] args) throws LoginException {
 
-        final String TOKEN = "MTE1MjM2MTI5ODg1MjU5Nzc5MA.Gu8K9I.o8KSS-KP24hjiSFVEIoSvDPkBiJF_ek5xj_pVs";
+        String clientSecret = ReadFile();
+        final String TOKEN = clientSecret;
         JDABuilder jdaBuilder = JDABuilder.createLight(TOKEN, GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS);
 
         jdaBuilder.setActivity(Activity.watching("Thursday Night Football"));
 
         jdaBuilder.addEventListeners(new MyListener()).build();
 
+    }
+
+    public static String ReadFile() {
+        try {
+            File newFile = new File("token.txt");
+            Scanner secretReader = new Scanner(newFile);
+            while (secretReader.hasNextLine()) {
+                String secret = secretReader.nextLine();
+                System.out.println("Client secret found: " + secret);
+                return secret;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred: is there a file named token.txt?");
+            e.printStackTrace();
+        }
+        return "balls"; // intellij made me do it
     }
 }
